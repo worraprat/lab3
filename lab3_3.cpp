9bfb420686dcd9c87425c581903fe7fde43024f2
lab302-26 <lab302-26@labeng.local>
2019-01-11 16:51:16 +0700

    test

--- lab3_3.cpp
+++ lab3_3.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int x=2;y=70;
+    float x=2;y=70;
     while(x<y){
         cout<< 1/x;
-        x=1/(x+1);
+        x=x+1;
 
     }
     cout<<"hello";
