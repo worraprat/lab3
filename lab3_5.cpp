54d14c08b4f711778d978ac6b3c964cd6da47f16
lab302-26 <lab302-26@labeng.local>
2019-01-11 17:31:44 +0700

    fuckingfinal

---
+++ lab3_5.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    string text1,text2;
+    int count=0,N;
+    cout<<"Enter the first text:";
+    cin>>text1;
+    cout<<"Enter the second text:";
+    cin>>text2;
+    cout<<"Enter N:";
+    cin>>N;
+    while(count<N){
+        if(count%2==0){
+            cout<<text1<<" " ;
+        }else(count%2==1);{
+            cout<<text2<<" ";
+            
+        }
+         count=count+1;
+    }
+    return 0;
+}
